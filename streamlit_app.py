5879ff211c1c8e1856f24c0923f76a4ba0bbe876
ANUSHAGORIJAVOLU <anusha.gorijavolu@gmail.com>
2025-07-31 16:09:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,9 @@ from snowflake.snowpark.functions import col
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
 name_on_order =st.text_input('Name On Smoothie :')
 
 ingredients_list=st.multiselect('Choose upto 5 items:',my_dataframe,max_selections=5)
